b3c28f15a2ad5b1358728e343eadcf4a7fe7bc87
Rafael <68511324+GalassoX@users.noreply.github.com>
2022-10-04 16:31:36 -0500

    fix: url checker change to more efficiency

--- shortener/index.js
+++ shortener/index.js
@@ -24,12 +24,11 @@ app.get('/api/hello', function (req, res) {
 
 app.post('/api/shorturl', (req, res) => {
   const { url } = req.body;
-  try {
-    const urldata = new URL(url);
-    const newUrlObject = { original_url: urldata.origin, short_url: URLS.length };
+  if (validURL(url)) {
+    const newUrlObject = { original_url: url, short_url: URLS.length + 1 };
     URLS.push(newUrlObject);
     res.json(newUrlObject);
-  } catch (e) {
+  } else {
     res.json({ error: 'Invalid url' });
   }
 });
@@ -43,6 +42,16 @@ app.get('/api/shorturl/:id', (req, res) => {
   const urlObject = URLS.find(u => u.short_url === parseInt(id));
   res.redirect(urlObject.original_url);
 });
+
+function validURL(str) {
+  const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+  return !!pattern.test(str);
+}
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
